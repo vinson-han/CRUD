9174a3ecf5ed7675c0d34f5791baabd1d0b6f773
vinson-han <han_vinson@student.smc.edu>
2024-01-08 19:59:06 -0800

    Create

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
 });
 
 app.listen(PORT, () => {
