7937f1708b75f1322992c0631273f38bb50deaa2
vinson-han <han_vinson@student.smc.edu>
2024-01-09 19:06:48 -0800

    mongodb

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { MongoClient } = require("mongodb");
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT;
+const uri = process.env.URI;
+const client = new MongoClient(uri);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -17,3 +19,19 @@ app.post("/users", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is live! Listening at port ${PORT}`);
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
